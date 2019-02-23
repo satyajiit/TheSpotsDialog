bda5dbc345c9703945569e7a007a5a331707aa0b
Satyajit Pradhan <satyajiit0@gmail.com>
2019-02-23 23:44:19 +0530

    Initial commit

---
+++ thespotsdialog/src/main/java/com/satyajit/thespotsdialog/AnimatorPlayer.java
@@ -0,0 +1,34 @@
+package com.satyajit.thespotsdialog;
+
+import android.animation.*;
+
+
+class AnimatorPlayer extends AnimatorListenerAdapter {
+
+    private boolean interrupted = false;
+    private Animator[] animators;
+
+    AnimatorPlayer(Animator[] animators) {
+        this.animators = animators;
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        if (!interrupted) animate();
+    }
+
+    void play() {
+        animate();
+    }
+
+    void stop() {
+        interrupted = true;
+    }
+
+    private void animate() {
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(animators);
+        set.addListener(this);
+        set.start();
+    }
+}
